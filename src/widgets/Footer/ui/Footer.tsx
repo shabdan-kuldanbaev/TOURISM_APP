99c7b17b8d2f12e259aca7b206b1e8d88ade43bc
shabdan-kuldanbaev <noxiousbrainiac@gmail.com>
2025-11-19 21:03:56 +0600

    styled footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,38 +1,40 @@
 import Link from 'next/link';
-import { FOOTER_LINKS } from '@/widgets/Footer/model/constants';
+import { FOOTER_LINKS } from '../model/constants';
 
 export const Footer = () => {
   return (
-    <footer className="bg-lime pt-[45vh] md:pt-[50vh] lg:pt-[60vh]">
-      <div className="z-0 fixed bottom-0 h-[45vh] md:h-[50vh] lg:h-[60vh] w-full pt-10 px-5 pb-5">
-        <div className="container mx-auto h-full flex flex-col justify-between">
-          <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
-            <div className="space-y-4">
-              <p className="text-white font-semibold text-lg">Gulzada Olzhobaeva</p>
-              <p className="text-white text-base opacity-90">
-                J'inspire les gens à voyager dans mon pays
-              </p>
+    <footer className="relative h-[60vh] bg-lime">
+      <div className="relative h-[calc(100vh+60vh)] -top-[100vh] bg-lime snap-y mandatory">
+        <div className="sticky top-[calc(100vh-60vh)] w-full pt-10 px-5 pb-5">
+          <div className="container mx-auto h-full flex flex-col justify-between">
+            <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
+              <div className="space-y-4">
+                <p className="text-white font-semibold text-lg">Gulzada Olzhobaeva</p>
+                <p className="text-white text-base opacity-90">
+                  J'inspire les gens à voyager dans mon pays
+                </p>
+              </div>
+
+              <div>
+                <ul className="space-y-2">
+                  {FOOTER_LINKS.map((link) => (
+                    <li key={link.link}>
+                      <Link
+                        href={link.link}
+                        className="text-white text-base opacity-80 hover:opacity-100 transition-opacity"
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
 
-            <div>
-              <ul className="space-y-2">
-                {FOOTER_LINKS.map((link) => (
-                  <li key={link.link}>
-                    <Link
-                      href={link.link}
-                      className="text-white text-base opacity-80 hover:opacity-100 transition-opacity"
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <p className="text-[70px] sm:text-[100px] md:text-[150px] lg:text-[200px] xl:text-[250px] text-center text-white font-bold font-custom">
+              La Kirghize
+            </p>
           </div>
-
-          <p className="text-[70px] sm:text-[100px] md:text-[150px] lg:text-[200px] xl:text-[250px] text-center text-white font-bold font-custom">
-            La Kirghize
-          </p>
         </div>
       </div>
     </footer>
